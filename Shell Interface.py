d2d19515680a1d758ab7f37dcc26a7e47a6f1c65
Robert Trad <roberttrad@gmail.com>
2020-04-12 13:44:43 +0300

    Refactored Shell Interface: Split setup from main

--- Shell Interface.py
+++ Shell Interface.py
@@ -1,41 +1,7 @@
 from STV import STV
 
 
-def formatname(v):
-    return '{:<20}'.format(v)
-
-
-def formatvote(v):
-    return '{:,.2f}'.format(v)
-
-
-def formatratio(v):
-    return '{:>5.0%}'.format(v)
-
-
-def print_lists(stv, voterid):
-    statcands = [('W', stv.winners), ('A', stv.active), ('D', stv.deactivated[::-1]), ('E', stv.excluded[::-1])]
-
-    for status, candidates in statcands:
-        for candidate in candidates:
-            print(formatname(candidate.name) + status, formatvote(candidate.votes))
-    print('------------')
-    print(formatname('Total Waste') + ' ', formatvote(stv.totalwaste))
-    
-    try:
-        voter = stv.voters[voterid]
-        print('\n' + voter.uid, 'list:')
-        for vl in voter.votelinks:
-            statusdescription = {vl.EXCLUDED: 'Excluded', vl.DEACTIVATED: 'Deactivated', vl.OPEN: 'Open',
-                                 vl.PARTIAL: 'Partial', vl.FULL: 'Full'}[vl.status]
-            print(formatname(vl.candidate.name), formatratio(vl.weight), '\t' + statusdescription)
-        print(formatname('Waste'), formatratio(voter.waste))
-    except KeyError:
-        pass
-
-
 def main():
-    # Setup
     print('Options:\n'
           '"r" to view the count by round or "l" by loop\n'
           '"g" to use group quotas\n'
@@ -45,23 +11,7 @@ def main():
     viewbyround = 'r' in viewmode or viewloop
     viewvoter = input('Please enter your unique id to see the progression of your vote: ')
 
-    # Fill Objects
-    with open('Area.csv', 'r') as f:
-        areaname, groups = f.readline().strip().split(',')
-        stv = STV(areaname, 'g' in viewmode, 'n' not in viewmode)
-        for group in groups.split(';'):
-            groupname, seats = group.split(':')
-            stv.add_group(groupname, int(seats))
-
-    with open('Candidates.csv', 'r') as f:
-        for line in f:
-            uid, name, groupname = line.strip().split(',')
-            stv.add_candidate(uid, name, groupname)
-
-    with open('Votes.csv', 'r') as f:
-        for line in f:
-            uid, votes = line.strip().split(',')
-            stv.add_voter(uid, votes)
+    stv = setup('g' in viewmode, 'n' not in viewmode)
 
     print('\nArea:', stv.areaname, '  Seats:', stv.totalseats, '\nTotal Votes:', len(stv.voters),
           '  Quota:', formatvote(stv.quota), '\n')
@@ -109,6 +59,64 @@ def main():
     print('Waste Percentage:', formatratio(stv.totalwaste / len(stv.voters)))
 
     input('\nPress any key to exit...')
+
+
+def setup(usegroups, reactivationmode):
+    """ Import from local files, create and return STV instance """
+
+    # Fill Objects
+    with open('Area.csv', 'r') as f:
+        areaname, groups = f.readline().strip().split(',')
+        stv = STV(areaname, usegroups, reactivationmode)
+        for group in groups.split(';'):
+            groupname, seats = group.split(':')
+            stv.add_group(groupname, int(seats))
+
+    with open('Candidates.csv', 'r') as f:
+        for line in f:
+            uid, name, groupname = line.strip().split(',')
+            stv.add_candidate(uid, name, groupname)
+
+    with open('Votes.csv', 'r') as f:
+        for line in f:
+            uid, votes = line.strip().split(',')
+            stv.add_voter(uid, votes)
+
+    return stv
+
+
+def print_lists(stv, voterid):
+    """ Prints candidate list and optionally, voter's ballot """
+    statcands = [('W', stv.winners), ('A', stv.active), ('D', stv.deactivated[::-1]), ('E', stv.excluded[::-1])]
+
+    for status, candidates in statcands:
+        for candidate in candidates:
+            print(formatname(candidate.name) + status, formatvote(candidate.votes))
+    print('------------')
+    print(formatname('Total Waste') + ' ', formatvote(stv.totalwaste))
+
+    try:
+        voter = stv.voters[voterid]
+        print('\n' + voter.uid, 'list:')
+        for vl in voter.votelinks:
+            statusdescription = {vl.EXCLUDED: 'Excluded', vl.DEACTIVATED: 'Deactivated', vl.OPEN: 'Open',
+                                 vl.PARTIAL: 'Partial', vl.FULL: 'Full'}[vl.status]
+            print(formatname(vl.candidate.name), formatratio(vl.weight), '\t' + statusdescription)
+        print(formatname('Waste'), formatratio(voter.waste))
+    except KeyError:
+        pass
+
+
+def formatname(v):
+    return '{:<20}'.format(v)
+
+
+def formatvote(v):
+    return '{:,.2f}'.format(v)
+
+
+def formatratio(v):
+    return '{:>5.0%}'.format(v)
 
 
 if __name__ == '__main__':
